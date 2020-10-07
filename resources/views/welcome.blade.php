f7aefdf72fe202c33ec9887df9cae8cd2d421498
Robaith <robaith@gmail.com>
2020-10-07 12:37:07 +0600

    ssl commerce and paypal client side

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+<div id="paypal-button"></div>
+<script src="https://www.paypalobjects.com/api/checkout.js"></script>
+<script>
+  paypal.Button.render({
+    // Configure environment
+    env: 'sandbox',
+    client: {
+      sandbox: 'ARvNMpdSrDOyKnkg7lNdSoLQcVksmijHM_FDuv17VJ6m6OwZxUY32ALEycGKTCpouw4VX3ly2eckxC1Q',
+      production: 'demo_production_client_id'
+    },
+    // Customize button (optional)
+    locale: 'en_US',
+    style: {
+      size: 'small',
+      color: 'gold',
+      shape: 'pill',
+    },
+
+    // Enable Pay Now checkout flow (optional)
+    commit: true,
+
+    // Set up a payment
+    payment: function(data, actions) {
+      return actions.payment.create({
+        transactions: [{
+          amount: {
+            total: '20.01',
+            currency: 'USD'
+          }
+        }]
+      });
+    },
+    // Execute the payment
+    onAuthorize: function(data, actions) {
+      return actions.payment.execute().then(function() {
+        // Show a confirmation message to the buyer
+        window.alert('Thank you for your purchase!');
+      });
+    }
+  }, '#paypal-button');
+
+</script>
